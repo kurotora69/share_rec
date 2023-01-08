53071cc2a509d02455da9a342df7f56e6edfa347
kurotora69 <yuka.bftt@gmail.com>
2023-01-08 16:58:03 +0900

    トップページ仮作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
-  # root to: "recreations#index"
-  resources :recreations, only: [:index]
+  root to: "recreations#index"
+  resources :recreations, only: :index
 end
