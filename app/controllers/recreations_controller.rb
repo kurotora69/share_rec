763d7ee8264a71b4330ff39328cd5d3863a051aa
kurotora69 <yuka.bftt@gmail.com>
2023-02-08 23:21:10 +0900

    コントローラーcreate処理

--- app/controllers/recreations_controller.rb
+++ app/controllers/recreations_controller.rb
@@ -4,6 +4,23 @@ class RecreationsController < ApplicationController
   end
 
   def new
+    @recreation = Recreation.new
   end
-    
+  
+  def create
+    @recreation = Recreation.new(recreation_params)
+    if @recreation.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+
+  private
+
+  def recreation_params
+    params.require(:recreation).permit(:genre_tag, :title, :explanation, :tool, :place, :posture, :staff_number, :care_number, :image).merge(user_id: current_user.id)
+  end  
+
 end
