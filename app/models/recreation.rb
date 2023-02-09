cb282838348c8118cb346031460d47e02bcdae47
kurotora69 <yuka.bftt@gmail.com>
2023-02-09 22:38:37 +0900

    バリデーション設定

--- app/models/recreation.rb
+++ app/models/recreation.rb
@@ -1,3 +1,14 @@
 class Recreation < ApplicationRecord
   has_one_attached :image
+  belongs_to :user
+
+  with_options presence: true do
+    validates :title
+    validates :explanation
+    validates :tool
+    validates :posture
+    validates :place
+    validates :staff_number
+    validates :care_number
+  end  
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :recreations
+  
+  with_options presence: true do
+    validates :nickname
+    validates :nursing_home
+    validates :occupation
+  end  
+
+
 end
